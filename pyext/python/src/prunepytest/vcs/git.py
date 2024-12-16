1304d8c499a5e14940a718e68a58a48252c5b39a
Hugues Bruant <hugues.bruant@gmail.com>
2024-12-16 14:46:44 -0800

    vcs/git: default to computing modified_files from fork-point
    Pick origin/master, origin/main, or some master/main from a remote
    as the best default candidates

--- pyext/python/src/prunepytest/vcs/git.py
+++ pyext/python/src/prunepytest/vcs/git.py
@@ -31,15 +31,67 @@ class Git(VCS):
             == 0
         )
 
-    def commit_id(self) -> str:
+    def commit_id(self, ref: str = "HEAD") -> str:
         return (
             subprocess.check_output(
-                ["git", "rev-parse", "HEAD"],
+                ["git", "rev-parse", ref],
                 stdin=subprocess.DEVNULL,
             )
             .decode("utf-8")
             .rstrip()
         )
+
+    def list_remotes(self) -> List[str]:
+        return (
+            subprocess.check_output(
+                ["git", "remote"],
+                stdin=subprocess.DEVNULL,
+            )
+            .decode("utf-8")
+            .rstrip()
+            .split()
+        )
+
+    def fork_point(
+        self, commit_id: str = "HEAD", from_ref: Optional[str] = None
+    ) -> Optional[str]:
+        try:
+            if from_ref is None:
+                remotes = self.list_remotes()
+                candidates = {}
+                for r in remotes:
+                    for cand in ("master", "main"):
+                        try:
+                            ref = f"{r}/{cand}"
+                            commit_id = self.commit_id(ref=ref)
+                            candidates[ref] = cand
+                        except subprocess.CalledProcessError:
+                            continue
+                if "origin/master" in candidates:
+                    from_ref = candidates["origin/master"]
+                elif "origin/main" in candidates:
+                    from_ref = candidates["origin/main"]
+                elif len(candidates) > 0:
+                    from_ref = next(iter(candidates.keys()))
+                else:
+                    print("failed to infer a viable fork point")
+                    return None
+
+            return (
+                subprocess.check_output(
+                    [
+                        "git",
+                        "merge-base",
+                        *(["--fork-point", from_ref] if from_ref else []),
+                        commit_id,
+                    ],
+                    stdin=subprocess.DEVNULL,
+                )
+                .decode("utf-8")
+                .rstrip()
+            )
+        except subprocess.CalledProcessError:
+            return None
 
     def dirty_files(self) -> List[str]:
         # NB: this *does* include untracked files
@@ -59,6 +111,8 @@ class Git(VCS):
     def modified_files(
         self, commit_id: str = "HEAD", base_commit: Optional[str] = None
     ) -> List[str]:
+        if base_commit is None:
+            base_commit = self.fork_point(commit_id)
         return list(
             itertools.chain.from_iterable(
                 # remove status letters, strip whitespaces, and split to catch both sides of renames
